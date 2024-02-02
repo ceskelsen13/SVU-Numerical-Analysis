de50e6d010f536e651bcdb6ab49e51eee73e86bf
Jared McBride <jared.mcbride@svu.edu>
2024-02-02 08:02:18 -0500

    Added Monte Calro integration implementation of log function

--- EquSolvers.jl
+++ EquSolvers.jl
@@ -1,5 +1,20 @@
 module EquSolver
 
+function MC_log(x; N = 1000)
+    count = 1
+
+    for i = 1: N
+        Y = rand()
+        X = (x-1)*rand() + 1
+
+        if Y < 1/X
+            count = count +1
+        end
+    end
+    I = count/N * (x-1)
+end
+
+
 ## Bisection method
 
 function hal(Dave)
