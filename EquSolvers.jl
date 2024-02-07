f97a778e1f82445ef7b18af5508af1ed2e85d330
Jared McBride <jared.mcbride@svu.edu>
2024-02-07 08:49:23 -0500

    Added Bisection method.

--- EquSolvers.jl
+++ EquSolvers.jl
@@ -15,11 +15,67 @@ function MC_log(x; N = 1000)
 end
 
 
-## Bisection method
-
 function hal(Dave)
     println("I can't do that, $Dave.")
 end
+
+## Bisection method
+
+"""
+Bisection method
+by Jared McBride (Feb 7, 2024)
+
+An implementation of the bisection method [Numerical Analysis 9e, Burden, Sec 2.1].
+
+"""
+function bisect_method(f,a,b; maxit = 50, tol = 1e-10)
+
+    i = 1               # Initialize counter
+    fa = f(a)           # Function value at left end
+    p = a +(b-a)/2      # Form is prefered to (a+b)/2
+
+    for i = 1:maxit
+
+        fp = f(p)
+        if fp == 0 || (b-a)/2 < tol     # check p is the zero or error is with in tolerance
+            break
+        end
+
+        if fa * fp > 0          # Check if fa and fp are different signs
+            a = p 
+            fa = fp
+        else
+            b = p
+        end
+    end
+
+    return p , i
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## Fixed point iterations
 
